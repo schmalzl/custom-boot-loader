58b52d6bef43c2393639e5c39c56a2dfee4b954f
kian-schmalzl <kian.schmalzl@icloud.com>
2025-08-27 15:35:50 +0200

    update bootloader

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### Overview
 This is a simple bootloader that anyone can use in their operating system. Just a bootloader that loads a kernel.cpp file.
 NOTE: The makefile is adapted to my needs. Make sure to adapt it to yours.
+
+NOTE 2: It currently only loads into text mode, so no graphics right now...
--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,4 +1,24 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+static inline void outb(uint16_t port, uint8_t val) {
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+// Set VGA 320x200x256 graphics mode
+extern "C" void set_graphics_mode() {
+    asm volatile (
+        "mov $0x13, %%ax\n\t"
+        "int $0x10\n\t"
+        :
+        :
+        : "ax"
+    );
+}
+
+// main entry point
 extern "C" void main(){
-    *(char*)0xb8000 = 'K';
-    return;
+    set_graphics_mode();
+    while (1);
 }
